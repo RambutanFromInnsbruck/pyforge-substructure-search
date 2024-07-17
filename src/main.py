6e96218ba0fbb271c84a9a244c870248d74a987c
RambutanFromInnsbruck <evgeny.fomichevsky@gmail.com>
2024-07-17 09:09:57 +0300

    Fixed the logic for empty result

--- src/main.py
+++ src/main.py
@@ -16,10 +16,13 @@ def substructure_search(mols: list, mol: str) -> list:
 
     if not match:
         print(f"The substructure '{mol}' is not found in any of the proposed molecules.")
-    return match
+    else:
+        return match
 
 
 substructure = 'c1ccccc1'
 molecules = ["CCO", "c1ccccc1", "CC(=O)O", "CC(=O)Oc1ccccc1C(=O)O"]
 result = substructure_search(molecules, substructure)
-print(f"The substructure '{substructure}' is found in the following molecules: {result}")
+
+if result is not None:
+    print(f"The substructure '{substructure}' is found in the following molecules: {result}")
